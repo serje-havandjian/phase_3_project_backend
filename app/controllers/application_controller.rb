0976d85bd2db2f76a41e8b6f860bec7cfe37a1d8
Serje <Serje.havandjian@gmail.com>
2022-06-21 09:53:36 -0700

    full CRUD on destination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,43 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Welcome to your mediocre travel site" }.to_json
   end
+
+  get "/destinations" do
+    destinations = Destination.destination_order
+    destinations.to_json
+
+  end
+
+  post "/destinations" do
+    new_destination = Destination.create(
+      destination: params[:destination],
+      geography: params[:geography],
+      rating: params[:rating],
+      country_id: params[:country_id],
+      tourist_id: params[:tourist_id]
+    )
+    new_destination.to_json
+  end
+
+  patch "/destinations/:id" do
+    update_destination = Destination.find(params[:id])
+    update_destination.update(
+      destination: params[:destination],
+      geography: params[:geography],
+      rating: params[:rating],
+      country_id: params[:country_id],
+      tourist_id: params[:tourist_id]
+    )
+    update_destination.to_json
+  end
+
+  delete "/destination/:id" do
+    delete_destination = Destination.find(params[:id])
+    delete_destination.destroy
+    delete_destination.to_json
+  end
+
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,8 @@
 class Destination < ActiveRecord::Base
     belongs_to :country
     belongs_to :tourist
+
+    def self.destination_order
+        self.all.order(:rating)
+    end
 end
